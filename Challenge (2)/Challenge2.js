fa2ad659537509ad227c1faded3a3e6823e3f544
unknown <surakhalaf379@gmail.com>
2023-03-28 06:03:28 +0300

    add files

---
+++ Challenge (2)/Challenge2.js
@@ -0,0 +1,20 @@
+var missingNumber = function(nums) {
+
+	if (nums[0] == 0 && nums.length == 1) {
+	    return 1;
+    }
+
+	if(nums.length == 1) {
+	    return nums[0]-1;
+    }
+
+	for (let n = 1; n <= nums.length + 1; n++) {
+		if (nums.includes(0)) {
+            if (nums.indexOf(n) == -1) { 
+                return n;
+            }
+        }
+		else
+		    return 0;
+    }
+};
